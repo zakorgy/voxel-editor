e482e9c9f4ad40045e83ae0500d43871099b9ef3
Attila Dusnoki <dati91@gmail.com>
2020-10-15 21:27:27 +0200

    Add toggle state to mesh grid

--- src/renderer.rs
+++ src/renderer.rs
@@ -88,6 +88,7 @@ pub struct Renderer {
     command_buffers: Vec<wgpu::CommandBuffer>,
     mesh_pipeline: Pipeline,
     render_cursor: bool,
+    render_mesh: bool,
     cursor_pipeline: Pipeline,
     voxel_pipeline: Pipeline,
     shadow_pipeline: Pipeline,
@@ -707,6 +708,7 @@ impl Renderer {
             cursor_cube,
             draw_cube: None,
             render_cursor: true,
+            render_mesh: true,
             mvp_buf: uniform_buf,
             multisampled_framebuffer,
             mesh_count,
@@ -740,6 +742,10 @@ impl Renderer {
             &self.mvp_buf,
             &mut self.command_buffers,
         );
+    }
+
+    pub fn toggle_render_mesh(&mut self) {
+        self.render_mesh = !self.render_mesh
     }
 
     fn get_grid_pos(world_pos: cgmath::Vector3<f32>) -> cgmath::Vector3<f32> {
@@ -1036,7 +1042,9 @@ impl Renderer {
                     clear_stencil: 0,
                 }),
             });
-            self.mesh_pipeline.draw(&mut rpass_depth);
+            if self.render_mesh {
+                self.mesh_pipeline.draw(&mut rpass_depth);
+            }
             if self.voxel_pipeline.instance_count > 0 {
                 self.voxel_pipeline.draw(&mut rpass_depth);
             }
